4f07d1d72f56ccee4947692b88ab486d90bf8e20
Daniel Gunn <dangunn@gmail.com>
2020-02-15 12:27:15 +0800

    Optimizing S2 using numba and numpy:
    before:
    Ran 1 test in 6.235s
    
    after:
    Ran 1 test in 0.708s

--- y2019/s2.py
+++ y2019/s2.py
@@ -1,29 +1,27 @@
+from numba import njit
+import numpy as np
+
 maxN = 1000000
 
-# Modified from https://www.geeksforgeeks.org/sieve-of-eratosthenes/
-def SieveOfEratosthenes(n):
-    # Create a boolean array "prime[0..n]" and initialize
-    #  all entries it as true. A value in prime[i] will
-    # finally be false if i is Not a prime, else true.
-    prime = [True for i in range(n + 1)]
-    p = 2
-    while (p * p <= n):
-
-        # If prime[p] is not changed, then it is a prime
-        if (prime[p] == True):
-
-            # Update all multiples of p
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-        p += 1
-
-    return prime
+# Using an optimized version of Sieve of Eratosthenes
+# Credits:  Giuseppe Vettigli 
+# https://dzone.com/articles/speeding-up-the-sieve-of-eratosthenes-with-numba
+@njit
+def sieve_python_jit(limit):
+    is_prime = np.full(limit, True)
+    is_prime[0] = False
+    is_prime[1] = False
+    for d in range(2, int(np.sqrt(limit) + 1)):
+        if is_prime[d]:
+            for n in range(d*d, limit, d):
+                is_prime[n] = False
+    return is_prime
 
 
 def main():
     t = int(input())
 
-    prime = SieveOfEratosthenes(maxN * 2)
+    prime = sieve_python_jit(maxN * 2)
 
     for i in range(t):
         n = int(input())
