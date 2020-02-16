56b3aeec4c5559cfa300472d109e261b504acb43
Daniel Gunn <dangunn@gmail.com>
2020-02-16 17:53:39 +0800

    Caching result of previous level for S5 to increase performance

--- y2019/s5.py
+++ y2019/s5.py
@@ -1,33 +1,35 @@
+import time
+
 def main():
-    s = input()
-    print(s)
-    s = s.split()
-    n = int(s[0])
-    k = int(s[1])
-    print(n,k)
+    n, k = map(int, input().split())
+    #print(n,k)
+
+    #stime = time.time()
+
     a = []
     for i in range(n):
-        r = input()
-        r = r.split()
-        r = list(map(int, r))
+        # 0 - this level
+        # 1 - previous level
+        r = [[int(x),int(x)] for x in input().split()]
         a.append(r)
 
+    this = 0
+    for level in range(2,k+1):
+        this = (this + 1) % 2
+        prev = (this + 1) % 2
+        for r in range(n-level,-1,-1):
+            for c in range(len(a[r])):
+                a[r][c][this] = max(a[r+1][c][prev], a[r+1][c+1][prev], a[r][c][this])
+
     sum = 0
-    # for each root
-    for r in range(n-k+1):
-        for c in range(r+1):
-            # for each sub-tree of size k
-
-            m =  a[r][c]
-            cl = 2
-            for r2 in range(r+1,r+k):
-                for c2 in range(c,c+cl):
-                    v = a[r2][c2]
-                    m = max(m, a[r2][c2])
-                cl += 1
-            sum += m
-
+    for r in range(0,n-k+1):
+        for c in a[r]:
+            sum += c[this]
     print(sum)
+
+    #etime = time.time()
+    #print("time:", (etime - stime))
+    # time: 57.030564069747925
 
 if __name__ == "__main__":
     main()
